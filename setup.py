6adeb0aa2e8b3ef9e3208e5e4924750490d091d4
AleksanderLidtke <alek_l@onet.eu>
2018-02-24 11:57:12 +0900

    Revert "Changed to use the newest version of wxWidgets, which need some prerequisites to be installed manually." to make the marge cleaner.
    This reverts commit ca3e5f7bf44da7c1d52c84a8ee6123ba88ec5a64.

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
 
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
 
-    install_requires=['wxpython>=4.0.1','pyserial','logging'],
+    install_requires=['wxpython>=3.0.2','pyserial','logging'],
 
     extras_require={
         'dev': ['pdb'],
