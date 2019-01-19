9270b2ba546b724a18333174e6cc60fca32e704d
AleksanderLidtke <alek_l@onet.eu>
2019-01-19 18:11:12 +0100

    Now also check message lengths. Added a new test - byte sequences.

--- tests/testGrabPortOutput_formatted.py
+++ tests/testGrabPortOutput_formatted.py
@@ -53,6 +53,9 @@ class Tests(unittest.TestCase):
 		formattedOutput=sm.commsInterface.grabPortOutput(self.fixture,'DummyBuff','formatted')
 		self.assertEqual(formattedOutput[0],'',msg='Expected empty string as output.')
 		self.assertEqual(formattedOutput[1],'DummyBuff',msg='Expected unchanged DummyBuff.')
+		# Check message length.
+		self.assertEqual(len(formattedOutput[0]),0,msg='Expected zero bytes')
+		self.assertEqual(len(formattedOutput[1]),9,msg='Expected nine bytes')
 		# Should have no warnings.
 		self.assertEqual(formattedOutput[2],{},msg='Expected empty warning dict.')
 		# The port should be empty now.
@@ -67,6 +70,9 @@ class Tests(unittest.TestCase):
 		self.assertEqual(formattedOutput[0],'',msg='Expected empty output.')
 		# 'formatted' outputFormat will append to the outputBuffer if there's no EOL termination.
 		self.assertEqual(formattedOutput[1],'DummyBuff\x00',msg='Expected DummyBuff\\x00.')
+		# Check message length.
+		self.assertEqual(len(formattedOutput[0]),0,msg='Expected zero bytes')
+		self.assertEqual(len(formattedOutput[1]),10,msg='Expected ten bytes')
 		# Should have no warnings.
 		self.assertEqual(formattedOutput[2],{},msg='Expected empty warning dict.')
 		# The port should be empty now.
@@ -79,6 +85,9 @@ class Tests(unittest.TestCase):
 		self.assertEqual(formattedOutput[0],'',msg='Expected empty output.')
 		# 'formatted' outputFormat will append to the outputBuffer if there's no EOL termination.
 		self.assertEqual(formattedOutput[1],'DummyBuff0',msg='Expected DummyBuff0.')
+		# Check message length.
+		self.assertEqual(len(formattedOutput[0]),0,msg='Expected zero bytes')
+		self.assertEqual(len(formattedOutput[1]),10,msg='Expected ten bytes')
 		# Should have no warnings.
 		self.assertEqual(formattedOutput[2],{},msg='Expected empty warning dict.')
 		# The port should be empty now.
@@ -93,6 +102,9 @@ class Tests(unittest.TestCase):
 		self.assertEqual(formattedOutput[0],'',msg='Expected empty output.')
 		# 'formatted' outputFormat will append to the outputBuffer if there's no EOL termination.
 		self.assertEqual(formattedOutput[1],'DummyBuff\x01',msg='Expected DummyBuff\\x01.')
+		# Check message length.
+		self.assertEqual(len(formattedOutput[0]),0,msg='Expected zero bytes')
+		self.assertEqual(len(formattedOutput[1]),10,msg='Expected ten bytes')
 		# Should have no warnings.
 		self.assertEqual(formattedOutput[2],{},msg='Expected empty warning dict.')
 		# The port should be empty now.
@@ -105,6 +117,9 @@ class Tests(unittest.TestCase):
 		self.assertEqual(formattedOutput[0],'',msg='Expected empty output.')
 		# 'formatted' outputFormat will append to the outputBuffer if there's no EOL termination.
 		self.assertEqual(formattedOutput[1],'DummyBuff1',msg='Expected DummyBuff1.')
+		# Check message length.
+		self.assertEqual(len(formattedOutput[0]),0,msg='Expected zero bytes')
+		self.assertEqual(len(formattedOutput[1]),10,msg='Expected ten bytes')
 		# Should have no warnings.
 		self.assertEqual(formattedOutput[2],{},msg='Expected empty warning dict.')
 		# The port should be empty now.
@@ -121,6 +136,9 @@ class Tests(unittest.TestCase):
 		# 'formatted' outputFormat will append to the outputBuffer if there's no EOL termination.
 		self.assertEqual(formattedOutput[1],'DummyBuff\x41',msg='Expected DummyBuff\\x41.')
 		self.assertEqual(formattedOutput[1],'DummyBuffA',msg='Expected DummyBuffA.') # Both representations work.
+		# Check message length.
+		self.assertEqual(len(formattedOutput[0]),0,msg='Expected zero bytes')
+		self.assertEqual(len(formattedOutput[1]),10,msg='Expected ten bytes')
 		# Should have no warnings.
 		self.assertEqual(formattedOutput[2],{},msg='Expected empty warning dict.')
 		# The port should be empty now.
@@ -134,6 +152,9 @@ class Tests(unittest.TestCase):
 		# 'formatted' outputFormat will append to the outputBuffer if there's no EOL termination.
 		self.assertEqual(formattedOutput[1],'DummyBuffA',msg='Expected DummyBuffA.')
 		self.assertEqual(formattedOutput[1],'DummyBuff\x41',msg='Expected DummyBuff\\x41.') # Both representations work.
+		# Check message length.
+		self.assertEqual(len(formattedOutput[0]),0,msg='Expected zero bytes')
+		self.assertEqual(len(formattedOutput[1]),10,msg='Expected ten bytes')
 		# Should have no warnings.
 		self.assertEqual(formattedOutput[2],{},msg='Expected empty warning dict.')
 		# The port should be empty now.
@@ -149,6 +170,9 @@ class Tests(unittest.TestCase):
 		self.assertEqual(formattedOutput[0],'DummyBuff\x00\n',msg='Expected DummyBuff\\x00\n.')
 		# No output buffer, we've sent one complete line.
 		self.assertEqual(formattedOutput[1],'',msg='Expected empty outputBuffer.')
+		# Check message length.
+		self.assertEqual(len(formattedOutput[0]),11,msg='Expected 11 bytes')
+		self.assertEqual(len(formattedOutput[1]),0,msg='Expected zero bytes')
 		# Should have no warnings.
 		self.assertEqual(formattedOutput[2],{},msg='Expected empty warning dict.')
 		# The port should be empty now.
@@ -161,6 +185,9 @@ class Tests(unittest.TestCase):
 		self.assertEqual(formattedOutput[0],'DummyBuff0\n',msg='Expected DummyBuff0\n.')
 		# No output buffer, we've sent one complete line.
 		self.assertEqual(formattedOutput[1],'',msg='Expected empty outputBuffer.')
+		# Check message length.
+		self.assertEqual(len(formattedOutput[0]),11,msg='Expected 11 bytes')
+		self.assertEqual(len(formattedOutput[1]),0,msg='Expected zero bytes')
 		# Should have no warnings.
 		self.assertEqual(formattedOutput[2],{},msg='Expected empty warning dict.')
 		# The port should be empty now.
@@ -176,6 +203,9 @@ class Tests(unittest.TestCase):
 		self.assertEqual(formattedOutput[0],'DummyBuff\x01\n',msg='Expected DummyBuff\\x01\n.')
 		# No output buffer, we've sent one complete line.
 		self.assertEqual(formattedOutput[1],'',msg='Expected empty outputBuffer.')
+		# Check message length.
+		self.assertEqual(len(formattedOutput[0]),11,msg='Expected 11 bytes')
+		self.assertEqual(len(formattedOutput[1]),0,msg='Expected zero bytes')
 		# Should have no warnings.
 		self.assertEqual(formattedOutput[2],{},msg='Expected empty warning dict.')
 		# The port should be empty now.
@@ -188,6 +218,9 @@ class Tests(unittest.TestCase):
 		self.assertEqual(formattedOutput[0],'DummyBuff1\n',msg='Expected DummyBuff1\n.')
 		# No output buffer, we've sent one complete line.
 		self.assertEqual(formattedOutput[1],'',msg='Expected empty outputBuffer.')
+		# Check message length.
+		self.assertEqual(len(formattedOutput[0]),11,msg='Expected 11 bytes')
+		self.assertEqual(len(formattedOutput[1]),0,msg='Expected zero bytes')
 		# Should have no warnings.
 		self.assertEqual(formattedOutput[2],{},msg='Expected empty warning dict.')
 		# The port should be empty now.
@@ -204,6 +237,9 @@ class Tests(unittest.TestCase):
 		self.assertEqual(formattedOutput[0],'DummyBuffA\n',msg='Expected DummyBuffA\n.') # Both representations work.
 		# No output buffer, we've sent one complete line.
 		self.assertEqual(formattedOutput[1],'',msg='Expected empty outputBuffer.')
+		# Check message length.
+		self.assertEqual(len(formattedOutput[0]),11,msg='Expected 11 bytes')
+		self.assertEqual(len(formattedOutput[1]),0,msg='Expected zero bytes')
 		# Should have no warnings.
 		self.assertEqual(formattedOutput[2],{},msg='Expected empty warning dict.')
 		# The port should be empty now.
@@ -215,6 +251,9 @@ class Tests(unittest.TestCase):
 		# output will have the input outputBuffer + sent byte + EOL.
 		self.assertEqual(formattedOutput[0],'DummyBuffA\n',msg='Expected DummyBuffA\n.')
 		self.assertEqual(formattedOutput[0],'DummyBuff\x41\n',msg='Expected DummyBuff\\x41\n.') # Both representations work.
+		# Check message length.
+		self.assertEqual(len(formattedOutput[0]),11,msg='Expected 11 bytes')
+		self.assertEqual(len(formattedOutput[1]),0,msg='Expected zero bytes')
 		# No output buffer, we've sent one complete line.
 		self.assertEqual(formattedOutput[1],'',msg='Expected empty outputBuffer.')
 		# Should have no warnings.
@@ -238,15 +277,65 @@ class Tests(unittest.TestCase):
 				# 'formatted' outputFormat will append to the outputBuffer if there's no EOL termination.
 				self.assertEqual(formattedOutput[1],'DummyBuff'+chr(i),
 					msg='Expected DummyBuff{}.'.format(chr(i)))
+				# Check message length.
+				self.assertEqual(len(formattedOutput[0]),0,msg='Expected zero bytes')
+				self.assertEqual(len(formattedOutput[1]),10,msg='Expected ten bytes')
 			else: # We terminated the message because 0x41='\n'
 				# Will move input outputBuffer to output and append nothing to it -
 				# only one complete line has been sent.
 				self.assertEqual(formattedOutput[0],'DummyBuff\n',msg='Expected DummyBuff\\n in output.')
 				self.assertEqual(formattedOutput[1],'',msg='Expected empty outputBuffer')
+				# Check message length.
+				self.assertEqual(len(formattedOutput[0]),10,msg='Expected ten bytes')
+				self.assertEqual(len(formattedOutput[1]),0,msg='Expected zero bytes')
 			# Should have no warnings.
 			self.assertEqual(formattedOutput[2],{},msg='Expected empty warning dict.')
 			# The port should be empty now.
 			self.assertEqual(self.fixture.read(1),b'',msg='Expected empty buffer after the test.')
+
+	def testFormattedGoodByte_fullASCIITableInOneGo(self):
+		""" Send the full ASCII table in many lines separated by EOLs. Some bytes
+		will be sent and read as hex codes of bytes, others as ASCII characters. """
+		previousDiffLen = self.maxDiff
+		self.maxDiff = None # To be able to see the entire message in case of test failure.
+
+		# The port should be empty now.
+		self.assertEqual(self.fixture.read(1),b'',msg='Expected empty buffer before the test.')
+
+		sentBytes=b'' # What we've actually sent.
+		ctr=10 # Sent EOL when this reaches 0.
+		for i in range(0,128): # From 0x00 (0) to 0x7F (127).
+			# Avoid implicit casting in the serial module - need to send bytes.
+			self.fixture.write(i.to_bytes(1,byteorder='big',signed=False))
+			sentBytes += i.to_bytes(1,byteorder='big',signed=False)
+			time.sleep(0.1) # In case there's a delay (to be expected on Windows).
+			ctr-=1
+			if ctr==0: # Send EOL every so often to split all characters into lines.
+				ctr=10
+				self.fixture.write(b'\n')
+				sentBytes += b'\n'
+		# Make sure there is a known outputBuffer after the message.
+		self.fixture.write(b'\nOutputBuffer') # No EOL after 'OutputBuffer'
+		sentBytes += b'\n' # 'OutputBuffer' will go to the outputBuffer, '\n' to output.
+
+		formattedOutput=sm.commsInterface.grabPortOutput(self.fixture,'DummyBuff','formatted')
+		# Will move input outputBuffer ('DummyBuff') to output and append the
+		# sent message to it. 'OutputBuffer' will now be in the outputBuffer.
+		self.assertEqual(formattedOutput[0],'DummyBuff'+''.join(chr(x) for x in sentBytes),
+			msg='Expected {} in output.'.format('DummyBuff'+''.join(chr(x) for x in sentBytes)))
+		self.assertEqual(formattedOutput[1],'OutputBuffer',msg="Expected\
+		 							'OutputBuffer' in outputBuffer")
+		# Check message length.
+		self.assertEqual(len(formattedOutput[0]),len('DummyBuff')+len(sentBytes),
+			msg='Expected {} bytes'.format(len('DummyBuff')+len(sentBytes)))
+		# print(''.join(chr(x) for x in sentBytes), # To eyeball the results.
+		# 	formattedOutput[0],formattedOutput[1])
+		# Should have no warnings.
+		self.assertEqual(formattedOutput[2],{},msg='Expected empty warning dict.')
+		# The port should be empty now.
+		self.assertEqual(self.fixture.read(1),b'',msg='Expected empty buffer after the test.')
+
+		self.maxDiff = previousDiffLen # Revert temporay change.
 
 	def testFormattedGoodByte_invalidASCII(self):
 		""" Send a formatted message with an invalid ASCII byte. """
@@ -284,7 +373,9 @@ class Tests(unittest.TestCase):
 		self.assertEqual(formattedOutput[0],'',msg='Expected empty output.')
 		# Will not change the outputBuffer if there's an invalid byte sent.
 		self.assertEqual(formattedOutput[1],'DummyBuff',msg='Expected unchanged DummyBuff.')
+		#FIXME Check message length when the bug is fixed.
 		# Should have two warnings.
+		#FIXME there's a bug here, SM will not detect how many invalid bytes it reads. It will just record a single error.
 		self.assertEqual(len(formattedOutput[2]),2,msg='Expected two warnings in the dict.')
 		print(formattedOutput[2]) # To eyeball the results.
 		# The port should be empty now.
@@ -299,7 +390,9 @@ class Tests(unittest.TestCase):
 		# output will be empty if there is no EOL termination of the message.
 		self.assertEqual(formattedOutput[0],'',msg='Expected empty output.')
 		# Should append the valid byte to outputBuffer (no EOL termination), and discard the invalid byte.
+		#FIXME there's a bug here, SM will discard an entire message if it containes an invalid byte.
 		self.assertEqual(formattedOutput[1],'DummyBuff\x7F',msg='Expected DummyBuff\\x7F.')
+		#FIXME Check message length when the bug is fixed.
 		# Should have one warning.
 		self.assertEqual(len(formattedOutput[2]),1,msg='Expected one warning in the dict.')
 		# print(formattedOutput[2]) # To eyeball the results.
@@ -315,7 +408,9 @@ class Tests(unittest.TestCase):
 		# output will be empty if there is no EOL termination of the message.
 		self.assertEqual(formattedOutput[0],'',msg='Expected empty output.')
 		# Should append the valid byte to outputBuffer (no EOL termination), and discard the invalid byte.
+		#FIXME there's a bug here, SM will discard an entire message if it containes an invalid byte.
 		self.assertEqual(formattedOutput[1],'DummyBuff\x7F',msg='Expected DummyBuff\\x7F.')
+		#FIXME Check message length when the bug is fixed.
 		# Should have one warning.
 		self.assertEqual(len(formattedOutput[2]),1,msg='Expected one warning in the dict.')
 		# print(formattedOutput[2]) # To eyeball the results.
@@ -323,16 +418,17 @@ class Tests(unittest.TestCase):
 		self.assertEqual(self.fixture.read(1),b'',msg='Expected empty buffer after the test.')
 
 	# port.inWaiting==0, should return the input outputBuffer - (empty dataStr)     DONE
-	#TODO test formatted output with:
+	# test formatted output with:
 		# 1) valid ASCII characters,                                                DONE
 		# 2) invalid ASCII characters,                                              BUG
 		# 3) valid numbers,                                                         DONE
 		# 4) empty dataStr,                                                         DONE
 		# 5) valid and invalid formatitng of the dataStr (bytes with and            DONE
 		# without EOL termination),
-		# 5) sequences of many bytes interrupted with \n.                           _
+		# 5) sequences of many bytes interrupted with \n.                           DONE
 	# Should try sending various representations of the same bytes to make          DONE
 	# sure they're all understood.
+	# Should check the length of the returned bytes.                                DONE
 
 if __name__ == '__main__':
 	unittest.main()
