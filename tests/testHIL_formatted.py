557ca8b149a506df998085b67ef3de78ba38c42b
AleksanderLidtke <alek_l@onet.eu>
2019-07-19 22:44:28 +0200

    Added HIL testing for hex output format, started working on formatted.

---
+++ tests/testHIL_formatted.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+""" Test the SerialMonitor.grabPortOutput by talking to an Arduino that is
+programmed to reply with known data to specific commands. This test verifies
+that the data are correctly received and interpreted.
+
+.. module:: SerialMonitor
+   :platform: Unix, Windows
+   :synopsis: Automated testing of the SerialMonitor with an Arduino.
+
+.. moduleauthor:: Alek, Artur
+
+"""
+import unittest, time
+import SerialMonitor as sm
+
+TEST_PORT = 'loop://' # Type of the test port. This one is a simple RX <-> TX
+	# type to be used for unit testing.
+	# https://pyserial.readthedocs.io/en/latest/url_handlers.html#loop
+
+TIMEOUT = 20 # Will wait for data to come from the Arduino TIMEOUT*100 ms.
+
+class Tests(unittest.TestCase):
+
+	def setUp(self):
+		""" Prepare resources for testing. """
+		import SerialMonitor as sm
+		import time
+
+		# Test port settings. Default and representative of what the SM does.
+		self.BaudRate = 9600
+		self.currentStopBits = sm.serial.STOPBITS_ONE
+		self.currentParity = sm.serial.PARITY_NONE
+		self.currentByteSize = sm.serial.EIGHTBITS
+
+		# See what ports we've got active.
+		ports = sm.commsInterface.getActivePorts()
+		if len(ports)<1:
+			raise BaseException('No active ports, connect the Arduino!')
+
+		# Open a port that we'll use to communicate with the Arduino. Assume it's
+		# the only currently connected device, hence use ports[0].
+		self.fixture = sm.serial.Serial(port=ports[0],
+										baudrate=self.BaudRate,
+										timeout=2,
+										stopbits=self.currentStopBits,
+										parity=self.currentParity,
+										bytesize=self.currentByteSize)
+
+		# Check that the port is readable.
+		time.sleep(2) # Need to let the things settle a bit. 1 second won't work.
+		if not sm.commsInterface.checkConnection(self.fixture):
+			self.fixture.close()
+			raise BaseException('Port {} is unreadable.'.format(self.fixture.port))
+
+		# Check that the Arduino replies with the expected message.
+		self.fixture.write(b'0') # Send the command byte.
+		timeoutCounter=0
+
+		while self.fixture.inWaiting() <= 0: # Wait for data to appear.
+			time.sleep(0.1)
+			timeoutCounter += 1
+			if timeoutCounter == TIMEOUT:
+				self.fixture.close()
+				raise BaseException('Getting test data from the Arduino on port {} timed out.'.format(self.fixture.port))
+
+		# Verify the reply to the command byte if no exception has been raised.
+		self.assertEqual(self.fixture.read(20),b'Arduino reachable.',
+						msg="Arduino hasn't replied with the expected string to the test command.")
+
+	def tearDown(self):
+		""" Done testing, get rid of the test resources."""
+		self.fixture.close()
+		del self.fixture
+
+	def testEmptyMessage(self):
+		""" Send an empty message. """
+		notNeeded=self.fixture.read(1) # Empty the port.
+		self.assertEqual(self.fixture.read(1),b'',
+						msg='Need an empty buffer before running this test case.')
+		# port.inWaiting will be 0, so grabPortOutput will just proceed to return
+		# the input outputBuffer and the default (empty) output.
+		hexOutput=sm.commsInterface.grabPortOutput(self.fixture,'DummyBuff','formatted')
+		self.assertEqual(hexOutput[0],'',msg='Expected empty string as output.')
+		self.assertEqual(len(hexOutput[0]),0,msg='Expected zero bytes.')
+		# 'hex' option should leave outputBuffer unchanged.
+		self.assertEqual(hexOutput[1],'DummyBuff',msg='Expected unchanged DummyBuff.')
+		# Should have no warnings.
+		self.assertEqual(hexOutput[2],{},msg='Expected empty warning dict.')
+		# The port should be empty now.
+		self.assertEqual(self.fixture.read(1),b'',msg='Expected empty buffer after the test.')
+
+		#TODO translate formatted tests into hardware in the loop.
+		
+if __name__ == '__main__':
+	unittest.main()
